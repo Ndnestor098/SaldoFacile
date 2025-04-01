dae0eeb92a6a6ecbb9504d223d8ed5cf155e4e67
ndnestor <trabajo.nestor.098@gmail.com>
2025-04-01 13:43:33 +0200

    started project

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,16 @@
+import { Head } from '@inertiajs/react';
+
+export default function Home() {
+    return (
+        <main className="">
+            <Head title="Home" />
+            <section>Image</section>
+            <section>
+                <header>
+                    <h1>Login</h1>
+                </header>
+                <form action=""></form>
+            </section>
+        </main>
+    );
+}
